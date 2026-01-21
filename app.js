7027b27cf295f3b19e7a9e9964f0c363902ab79a
root <root@ip-172-31-38-233.eu-north-1.compute.internal>
2026-01-21 09:47:05 +0000

    Test auto deployment

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-<<<<<<< HEAD
-  res.end('Hello Friends we;come to My page.Thank you🚀');
-=======
   res.end('Hello Friends, Welcome to My Page 🚀');
->>>>>>> 7400402825a38192d711cc37023d2b621b73bddb
 });
 
 server.listen(3000, () => {
