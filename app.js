bb80dd41a5ae39268ea099596d526a6245ba6902
root <root@ip-172-31-38-233.eu-north-1.compute.internal>
2026-01-21 07:20:56 +0000

    Add Node app and CodeDeploy files

---
+++ app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Biki Sports – Thank you for visiting my page 🚀');
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
+
